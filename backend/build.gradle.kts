a95b894dfa9871c276e7638a8e6ada21687c7678
buihuy13 <buiquochuytd@gmail.com>
2026-02-01 04:13:41 +0000

    init backend

---
+++ backend/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.2" apply false
+	id("io.spring.dependency-management") version "1.1.7" apply false
+	id("com.diffplug.spotless") version "6.25.0" apply false
+}
+
+allprojects {
+	apply(plugin = "java")
+	apply(plugin = "com.diffplug.spotless")
+
+	java {
+		toolchain {
+			languageVersion = JavaLanguageVersion.of(21)
+		}
+	}
+
+	group = "com.CNTTK18"
+	version = "0.0.1-SNAPSHOT"
+	description = "Restaurant Website"
+
+	repositories {
+        mavenCentral()  
+    }
+}
+
+subprojects {
+	apply(plugin = "io.spring.dependency-management")
+	apply(plugin = "com.diffplug.spotless")
+
+	configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+		java {
+			palantirJavaFormat()
+			target("src/**/*.java")
+			removeUnusedImports()
+			importOrder("java", "jakarta", "org", "com", "")
+		}
+	}
+
+	configurations {
+	    compileOnly {
+		    extendsFrom(configurations.annotationProcessor.get())
+	    }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
