a7cc99c1f865e62ca8dc913049b1fc51645fc7a9
buihuy13 <buiquochuytd@gmail.com>
2026-02-20 06:43:01 +0000

    routing user-service, chat-service and notification-service

--- backend/chat-service/build.gradle.kts
+++ backend/chat-service/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.mapstruct:mapstruct:1.6.3")
 	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
 }
 
 dependencyManagement {
