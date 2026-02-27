d46772915705e113d66c57b1dbecf95716bb6314
buihuy13 <buiquochuytd@gmail.com>
2026-02-27 07:48:03 +0000

    added caddy, deploy files, and pre deploy files

---
+++ deploy/docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+command=$1
+
+if [ "$command" = "up" ]; then
+  docker compose -f docker-compose.prod.yml up -d
+  echo "Waiting for Keycloak to be ready..."
+  #Sửa host khi deploy
+  until curl -s "http://localhost:9090/health/ready" | grep -q "UP"; do
+    echo "Keycloak not ready yet, retrying in 3s"
+    sleep 3
+  done
+
+  echo "Waiting for realm to be ready..."
+  #Sửa host khi deploy
+  until curl -s "http://localhost:9090/realms/restaurant-realm" | grep -q "restaurant-realm"; do
+    echo "Realm not ready yet, retrying in 3s..."
+    sleep 3
+  done
+
+  echo "Running role management script"
+  bash ./keycloak-config/role-management.sh
+else
+  echo "Usage: $0 {up}"
+  exit 1
+fi
