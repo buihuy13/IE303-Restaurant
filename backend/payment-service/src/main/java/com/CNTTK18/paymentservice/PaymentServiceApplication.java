800b73094c9e9da34118154d98b583f0ca47ca8c
vanthinh26102005 <vangvanminh.261005@gmail.com>
2026-03-06 10:49:48 +0700

    fix lint

--- backend/payment-service/src/main/java/com/CNTTK18/paymentservice/PaymentServiceApplication.java
+++ backend/payment-service/src/main/java/com/CNTTK18/paymentservice/PaymentServiceApplication.java
@@ -1,9 +1,10 @@
 package com.CNTTK18.paymentservice;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
 @OpenAPIDefinition(servers = {@Server(url = "${gateway.url}")})
