8f75e0e0fec5cce173fc761efc22f1394b04e092
vanthinh26102005 <vangvanminh.261005@gmail.com>
2026-03-06 10:48:19 +0700

    add swagger config

--- backend/payment-service/src/main/java/com/CNTTK18/paymentservice/PaymentServiceApplication.java
+++ backend/payment-service/src/main/java/com/CNTTK18/paymentservice/PaymentServiceApplication.java
@@ -1,11 +1,12 @@
 package com.CNTTK18.paymentservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@OpenAPIDefinition(servers = {@Server(url = "${gateway.url}")})
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
