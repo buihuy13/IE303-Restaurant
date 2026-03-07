fc4e8a908756e17b5fddbdfb3df7561666c25374
huutri <huutri2701@gmail.com>
2026-03-07 22:26:52 +0700

    build core business

---
+++ backend/blog-service/src/main/java/com/CNTTK18/blog_service/mapper/BlogMapper.java
@@ -0,0 +1,26 @@
+package com.CNTTK18.blog_service.mapper;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import com.CNTTK18.blog_service.dto.response.BlogResponse;
+import com.CNTTK18.blog_service.model.BlogPost;
+
+@Mapper(componentModel = "spring")
+public interface BlogMapper {
+    @Mapping(target = "publishedAt", source = "publishedAt", qualifiedByName = "convertToVNZone")
+    @Mapping(target = "createdAt", source = "createdAt", qualifiedByName = "convertToVNZone")
+    @Mapping(target = "updatedAt", source = "updatedAt", qualifiedByName = "convertToVNZone")
+    BlogResponse toBlogResponse(BlogPost blogPost);
+
+    @Named("convertToVNZone")
+    default ZonedDateTime convertToVNZone(Instant instant) {
+        if (instant == null) return null;
+        return instant.atZone(ZoneId.of("Asia/Ho_Chi_Minh"));
+    }
+}
