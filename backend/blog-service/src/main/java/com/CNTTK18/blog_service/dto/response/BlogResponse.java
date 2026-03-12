30e010ec74d6e039886c405a26cacb39493ee82c
huutri <huutri2701@gmail.com>
2026-03-12 11:39:36 +0700

    update field publicID into private

--- backend/blog-service/src/main/java/com/CNTTK18/blog_service/dto/response/BlogResponse.java
+++ backend/blog-service/src/main/java/com/CNTTK18/blog_service/dto/response/BlogResponse.java
@@ -24,7 +24,6 @@ public class BlogResponse {
     private String slug;
     private String content;
     private String coverImageUrl;
-    private String publicID;
     private BlogStatus status;
 
     @JsonSerialize(using = ZonedDateTimeSerializer.class)
