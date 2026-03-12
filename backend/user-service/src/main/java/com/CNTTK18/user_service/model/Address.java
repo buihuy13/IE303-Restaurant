a95f86095fa6d69f900d749fd77319688743eb24
Bùi Quốc Huy <145268235+buihuy13@users.noreply.github.com>
2026-03-12 16:17:11 +0700

    handled entitylisteners in reviews and user

--- backend/user-service/src/main/java/com/CNTTK18/user_service/model/Address.java
+++ backend/user-service/src/main/java/com/CNTTK18/user_service/model/Address.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -13,6 +14,7 @@ import jakarta.persistence.Table;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,6 +29,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class Address {
     @Id
     private UUID id;
