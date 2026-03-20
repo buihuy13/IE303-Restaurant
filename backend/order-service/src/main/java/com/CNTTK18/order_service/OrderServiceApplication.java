feb7c4bd6a53dbaf62bad6d99add217cbb91070e
Cao Trình Thịnh <148177550+caotrinhthinh@users.noreply.github.com>
2026-03-20 13:53:35 +0700

    style: apply spotless formatting and cleanup imports

--- backend/order-service/src/main/java/com/CNTTK18/order_service/OrderServiceApplication.java
+++ backend/order-service/src/main/java/com/CNTTK18/order_service/OrderServiceApplication.java
@@ -2,7 +2,6 @@ package com.CNTTK18.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
