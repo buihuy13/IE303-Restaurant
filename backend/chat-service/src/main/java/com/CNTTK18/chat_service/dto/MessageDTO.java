fe38173a717667c1d796f0d21ff5cd1dc0bb53b7
buihuy13 <buiquochuytd@gmail.com>
2026-04-07 13:39:31 +0000

    fixed validation annotation

--- backend/chat-service/src/main/java/com/CNTTK18/chat_service/dto/MessageDTO.java
+++ backend/chat-service/src/main/java/com/CNTTK18/chat_service/dto/MessageDTO.java
@@ -4,7 +4,7 @@ import java.time.Instant;
 import java.util.UUID;
 
 import jakarta.validation.constraints.NotBlank;
-
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,10 +17,10 @@ import lombok.Setter;
 public class MessageDTO {
     private UUID roomId;
 
-    @NotBlank
+    @NotNull
     private UUID senderId;
 
-    @NotBlank
+    @NotNull
     private UUID receiverId;
 
     @NotBlank
