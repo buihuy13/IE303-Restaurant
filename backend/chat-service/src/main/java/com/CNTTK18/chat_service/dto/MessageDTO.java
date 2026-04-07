c83e137c8b7f0f3acd9434decd6365775ddd55d1
buihuy13 <buiquochuytd@gmail.com>
2026-04-07 13:40:12 +0000

    spotless apply

--- backend/chat-service/src/main/java/com/CNTTK18/chat_service/dto/MessageDTO.java
+++ backend/chat-service/src/main/java/com/CNTTK18/chat_service/dto/MessageDTO.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
