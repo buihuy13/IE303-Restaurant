4747279cc6b3457836d554ae4fd8e4547328ad3d
Bùi Quốc Huy <145268235+buihuy13@users.noreply.github.com>
2026-04-23 16:18:50 +0700

    mood based food recommendation
    * Supported notification publish with sse for order status
    
    * spot.sh
    
    * recommended food base on mood

--- backend/recommendation-service/src/main/java/com/CNTTK18/recommendation_service/service/RecommendationService.java
+++ backend/recommendation-service/src/main/java/com/CNTTK18/recommendation_service/service/RecommendationService.java
@@ -2,12 +2,15 @@ package com.CNTTK18.recommendation_service.service;
 
 import java.util.List;
 
+import com.CNTTK18.recommendation_service.dto.request.MoodFoodRecommendationRequest;
 import com.CNTTK18.recommendation_service.dto.request.ReviewRequest;
 import com.CNTTK18.recommendation_service.dto.response.MessageResponse;
 import com.CNTTK18.recommendation_service.dto.response.ReviewSummarizeResponse;
 
 public interface RecommendationService {
     public MessageResponse recommendFood(String userContext);
+
+    public MessageResponse recommendFoodByMood(MoodFoodRecommendationRequest request);
 
     public List<MessageResponse> generateFoodDescription(String foodName);
 
