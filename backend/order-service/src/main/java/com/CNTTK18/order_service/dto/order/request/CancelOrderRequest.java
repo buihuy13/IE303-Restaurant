5e9d8c3dfd7cc9104a2ce6b355afa8a89c5a1156
Cao Trình Thịnh <148177550+caotrinhthinh@users.noreply.github.com>
2026-05-19 20:17:28 +0700

    chore(order): reformat CancelOrderRequest

--- backend/order-service/src/main/java/com/CNTTK18/order_service/dto/order/request/CancelOrderRequest.java
+++ backend/order-service/src/main/java/com/CNTTK18/order_service/dto/order/request/CancelOrderRequest.java
@@ -1,7 +1,8 @@
 package com.CNTTK18.order_service.dto.order.request;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
